81ade4679b2d142489202aac49dbd61b88f4868e
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-16 12:37:24 +0530

    javascript basics

--- js/01-js-intro.js
+++ js/01-js-intro.js
@@ -1,5 +1,36 @@
 
-console.log('Hello world!');
+// console.log('Hello world!');
+// console.log("Hello world!");
+// console.log(`Hello world!`);
+
+// console.log(10 + 20);
+
+// primitive datatypes in JS 
+// string number boolean 
+
+// variable declaration in JS
+
+// Java
+// int num = 10;
+
+// JS 
+// num = 10; // do not use this 
+// var num2 = 20; // do not use this 
+// let num3 = 30; // use this for variables 
+// const num4 = 40; // use this for constant variables (final in Java)
+
+// JS - dynamically types language 
+
+// let num = 10;
+
+let myData = 10;
+
+console.log(myData);
+console.log(typeof (myData));
+
+
+
+
 
 
 
