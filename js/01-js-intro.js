f190785cc5bb365e9ec9be99bce8a27681c9d5c9
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-16 16:29:56 +0530

    javascript basics

--- js/01-js-intro.js
+++ js/01-js-intro.js
@@ -85,11 +85,14 @@
 const empData = {
     eid: 101,
     firstName: 'Sonu',
-    salary: 10.5
+    salary: 10.5,
+    isMarried: false,
+    phones: [9876543210, 6785431209],
+    address: { city: 'Hyderabad', pin: 500001 }
 };
 
-console.log(empData);
-
+// console.log(empData);
+console.log(empData.address.city);
 
 
 
