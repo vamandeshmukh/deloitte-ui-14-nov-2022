a3a3ea8030fdd0b2148fcdd62941e55a1c4a4099
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-17 09:54:54 +0530

    js

--- js/04-js-callbacks.js
+++ js/04-js-callbacks.js
@@ -2,3 +2,30 @@
 // callbacks in js 
 
 // a function can be passed a argument to another function 
+
+
+// enter a vaule 
+// let someVariable = ???;
+
+// const fun = (arg) => {
+//     // code 
+// }
+// fun(??);
+
+// const fun = (arg) => {
+//     console.log('fun function');
+//     // arg++;
+//     let myCity = arg.city;
+//     console.log(myCity);
+// }
+// fun(10);
+// fun({ city: 'Hyderabad', pin: 500001 });
+
+const fun = (arg) => {
+    console.log('fun function');
+    arg();
+}
+
+fun();
+
+
