b6e8fba92eed5b37ddfdd59fe0ad060c4d803f4b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-18 12:15:45 +0530

    js

--- js/99-customer-script.js
+++ js/99-customer-script.js
@@ -22,11 +22,25 @@ registeredCustomers[0] = new Customer(101, 'Sonu', [9876543210, 6789543210], 'so
 registeredCustomers[1] = new Customer(102, 'Monu', [8976543210, 7896543210], 'monu@deloitte.com', { street: 'Meenakshi Towers', city: 'Hyderabad', 'pin': 500081 });
 registeredCustomers[2] = new Customer(101, 'Tonu', [9988776655, 9988776655], 'tonu@deloitte.com', { street: 'Whites road', city: 'Chennai', 'pin': 600006 });
 
-registeredCustomers.forEach(e => {
-    console.log(e);
-    const divElem = document.createElement('div');
-    const divElemData = document.createTextNode(`${e.customerId} ${e.firstName} ${e.phones[0]} ${e.phones[1]} ${e.email} ${e.address.city} ${e.address.street} ${e.address.pin}`);
-    divElem.appendChild(divElemData);
-    document.body.appendChild(divElem);
-});
+
+    registeredCustomers.forEach(e => {
+        console.log(e);
+        const divElem = document.createElement('div');
+        const divElemData = document.createTextNode(`${e.customerId} ${e.firstName} ${e.phones[0]} ${e.phones[1]} ${e.email} ${e.address.city} ${e.address.street} ${e.address.pin}`);
+        divElem.appendChild(divElemData);
+        document.body.appendChild(divElem);
+    });
+
+
+const saveCustDetails = () => {
+    const newCustomer = new Customer();
+    newCustomer.customerId = document.getElementById('customerId').value;
+    registeredCustomers[registeredCustomers.length] = newCustomer;
+    document.getElementById('custDataForm').reset();
+    // document.getElementById('custData').innerHTML = `${newCustomer.customerId} ${newCustomer.firstName} ${newCustomer.phones[0]} ${newCustomer.email} ${newCustomer.address.city}`;
+    console.log(newCustomer.customerId);
+    console.log(registeredCustomers.length);
+
+}
+
 
