9547ab6f5786d102c820a9f8efb5b7ea3bfb393d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-18 09:59:49 +0530

    js

--- js/00-delete.js
+++ js/00-delete.js
@@ -1,6 +1,4 @@
 
-
-
 const array1 = [1, 30, 4, 21, 100000];
 // array1.sort();
 array1.sort((a, b) => { return a - b; });
---
+++ js/07-js-oop.js
@@ -0,0 +1,8 @@
+
+// console.log('OOP concepts in JS');
+
+const userName = 'Vaman';
+// const greetingMessage = 'Hi ' + userName + '!'; // this line 
+const greetingMessage = `Hi ${userName}!`; // this line 
+
+console.log(greetingMessage);
